259e9a23b29f44876d5fc62155aed4adea0ec834
kimyk0120 <kimyk0120@hotmail.com>
2019-01-07 13:12:52 +0900

    exm

--- bookEx00.py
+++ bookEx00.py
@@ -1,11 +1,48 @@
+
+# 모두의 파이썬
 # import turtle as t
-#
-# t.shape("turtle")
-# t.forward(50)
+#t.shape("turtle")
+#t.forward(50)
 
-for x in range(10):
-    print("for test")
+# for x in range(10):
+#     print("for test")
 
-# vs code 사용시 view - terminal 에서 python3 (파일이름) 으로 실행 
+# vs code 사용시 view - terminal 에서 python3 (파일이름) 으로 실행
+
+# list(range(5))
+# # print(list)
+
+# s = 0 
+# for x in range(1,10+1):
+#     s = s+x
+#     print("x : " ,x ," sum :", s)
+
+# angle = 89
+# t.bgcolor("black")
+# t.color("yellow")
+# t.speed(0)
+# for x in range(200):
+#     t.forward(x)
+#     t.left(angle)
+
+# name = input("name? ")
+# print(name)
+
+# def factorial(n):
+#     fact = 1
+#     for x in range(1,n+1):
+#         fact = fact * x 
+#         print(fact)
+#     return fact
+
+# print(factorial(10))
+
+
+# s = 1 
+# for x in range(1,5):
+#     s = s * x 
+#     print(s)
+
+
 
 
--- facamEx.py
+++ facamEx.py
@@ -8,3 +8,16 @@
 # a['a'] = 11
 # print(a)
 
+
+
+# 조건문 if 
+# payment = 'complete'
+# payment = 'test'
+# if payment == 'complete':
+#     print("complete")
+# elif payment == 'inprogress':
+#     print("inprogress")
+# else:
+#     print('nope')
+
+
