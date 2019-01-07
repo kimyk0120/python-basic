49e4f32b01d36cf2abfba85f0b6b3a8baf40765b
kimyk0120 <kimyk0120@hotmail.com>
2019-01-07 14:09:14 +0900

    fa cam

--- facamEx.py
+++ facamEx.py
@@ -8,8 +8,6 @@
 # a['a'] = 11
 # print(a)
 
-
-
 # 조건문 if 
 # payment = 'complete'
 # payment = 'test'
@@ -19,5 +17,45 @@
 #     print("inprogress")
 # else:
 #     print('nope')
+
+# first =  'somthing'
+# second = ''
+# if first:
+#     if second:
+#         print("test2")
+#     print("test1")
+
+# list_a = [1,2]
+# if 1 in list_a:
+#     print("test")
+
+# if 3 in list_a:
+#     print("test")
+
+# if 1 in list_a or 3 in list_a:
+#     print("test")
+
+
+# 반복문 for
+# list_a = [1,2,3,5]
+# for looper in list_a:
+#     print(looper)
+# for looper in range(10):
+#     print(looper)
+
+# for key in dict_val.keys():
+#     print(key)
+# for key in dict_val.keys():
+#     print(dict_val[key])
+# for val in dict_val.values():
+#     print(val)
+# dict_val = {"a":1,"b":2}
+# for(key, val) in dict_val.items(): # 튜플로 반환 
+#     print(key)
+#     print(val)
+
+# 반복문 while
+
+
 
 
