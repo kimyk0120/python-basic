5a683df192b24c7a001ff89f351823fd720e7031
kimyk0120 <kimyk0120@hotmail.com>
2019-01-08 09:34:17 +0900

    python-basic

--- facamEx.py
+++ facamEx.py
@@ -8,6 +8,7 @@
 # a['a'] = 11
 # print(a)
 
+
 # 조건문 if 
 # payment = 'complete'
 # payment = 'test'
@@ -54,7 +55,54 @@
 #     print(key)
 #     print(val)
 
+
 # 반복문 while
+# data_a = ['data1','data2','data3','data4']
+# while data_a:
+#     print(data_a.pop())
+#
+# print(data_a)
+
+# for looper in [0,1,2]:
+#     print(looper)
+#     continue
+#     print("continue")
+
+# while True:
+#     print("test")
+#     break
+#     print("test1")
+
+# for num in range(100):
+#     if num > 50 :
+#         break
+#     if num % 2 ==0:
+#         continue
+#     print(num)
+
+
+# 함수
+# def testFunc(a,b="2"):
+#     print(a, " " ,b)
+# testFunc("a","3")
+
+# def print_name(a,b=2):
+    # print("test")
+    # print("test2\n")
+    # return a+b
+
+# print(print_name(1,4))
+# print(print_name(12,43))
+# print(print_name(14))
+
+
+
+
+
+
+
+
+
 
 
 
