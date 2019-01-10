1e3d6a3e371cf4f84eb546c7e6418acedb2e6c70
kimyk0120 <kimyk0120@hotmail.com>
2019-01-10 11:39:17 +0900

    ;python-basic

--- facamEx.py
+++ facamEx.py
@@ -87,9 +87,9 @@
 # testFunc("a","3")
 
 # def print_name(a,b=2):
-    # print("test")
-    # print("test2\n")
-    # return a+b
+#     print("test")
+#     print("test2\n")
+#     return a+b
 
 # print(print_name(1,4))
 # print(print_name(12,43))
@@ -122,21 +122,116 @@
 #     send_mail("me",user['email'],content)
 
 
-# 14. 내장함수
+'''
+    14.내장함수
+'''
+# a = '2'
+# a = int(a)
+# print(a)
+
+# str_va = '100'
+# a= int(str_va)+1
+# print(a)
+#
+# print(type(str_va))
+
+# int_val = 100
+# a = str(int_val)
+# print(a)
+# print(type(a))
+# a += " test"
+# print(a)
+
+# str_var = 'test'
+# tuple_var = (1,2,3)
+# print(list(str_var)) #['t', 'e', 's', 't']
+# print(list(tuple_var))
+#
+# list_var = [1,2,3]
+# print(tuple(list_var))
+#
+# int_val = 100
+# print(list(int_val)) # 'int' object is not iterable
+
+# list_val = range(3)
+# print(list_val)
+
+# a = 'test'
+# print(len(a))
+# a = [1,2,3]
+# print(len(a))
+# a = {"2":2}
+# print(len(a))
+
+# for idx in range(5):
+#     print(idx)
+
+# a = [1,2,3,4]
+# print(max(a))
+# print(min(a))
 
 
+'''
+    15. class
+'''
+
+# class Test():
+#
+#     def __init__(self):
+#         test_val = ''
+#
+#     def test_val_print(self):
+#         print("test")
+#
+# test = Test()
+# test.test_val_print()
 
 
+'''
+    16. class 
+'''
 
+# class Test():
+#
+#     test_val2 = "test_val2"
+#
+#     def __init__(self):
+#         self.test_val = ''
+#
+#     def test_val_print(self,a):
+#         print(self.test_val , a)
+#     def print_textval2(self):
+#         print(self.test_val2)
+#
+# test = Test()
+# test.test_val = "test2"
+# test.test_val_print("test3")
+# test.print_textval2()
 
+# class Test():
+#     def __init__(self):
+#         self.myData = 100
+#         print("__init called")
+#
+# test = Test()
+# test2 = Test()
+# print(test.myData)
+# print(test2.myData)
+# test2.myData = 10
+# print(test2.myData)
 
-
-
-
-
-
-
-
+# class SimpleTest():
+#     def print_skip(self,str):
+#         if 'skip' in str:
+#             print("skip")
+#             return
+#
+#         print(str)
+#
+# simple = SimpleTest()
+# simple.print_skip("ddad")
+# simple.print_skip("skip text")
+# simple.print_skip("ssfsdf")
 
 
 
--- jumpToPythonEx.py
+++ jumpToPythonEx.py
@@ -35,6 +35,158 @@
 # b= "Test formatting string : %s"
 # print(b % 'kim')
 
+# c = 5
+# a = "Test formatting number : %d"
+# print(a % c)
+# d = 'text'
+# a = "Test formatting n,s : %d %s"
+# print(a % (c,d))
+
+# b  = 5
+# c = 3.42
+# a = "Test formatting: %s , %s"
+# print(a % (b,c))
+
+# a = "Error is %d%%"
+# print(a % 56)
+
+# print("%10s" % 'test')
+# print("%10s" % 'test1',"%-10s" % 'test2' )
+
+# a = 'tessst'
+# print(a.count('s')) # 해당 텍스트 개수 반환
+# print(a.find('s'))
+# print(a.find('k'))
+# print(a.index('s'))
+# print(a.index('k'))
+
+# a = ','
+# b = a.join('abcd')
+# print(b)
+
+# a = 'Hello'
+# print(a.upper());
+# print(a.lower());
+
+# a = "  hi  ";
+# print(a.rstrip()); print(a.lstrip()); print(a.strip())
+
+# a = ' a replace b '
+# a = a.replace('a','c'); a = a.replace('b', 'd')
+# print(a)
+
+# a = 'a, b, c, d'
+# b = a.split(',')
+# print(b)
+
+# a = ['a','b','c']
+# b= len(a)
+# print(b)
+# print(a[b-1])
+# print(a[-1])
+
+# a = [1, 2, 3, [4, 5, 6]]
+# print(a[-1][0])
+
+# a = [1,2,3,4,5]
+# print(a[2:4]) # 2번부터 3번까지
+
+# a = [1,4,3]
+# b = [4,5,6]
+# print(a+b) #[1, 4, 3, 4, 5, 6]
+
+# a = [1,2,3]
+# print(a*3)
+# print('test'+str(a[2]) , ' type : ', type(a[0]), type(str(a[2])))
+# a[2] = 4
+# print(a)
+# print(a[1:2]) # 1번부터 1번까지
+# # a[1:2] = ['a','b','c']
+# # print(a)
+# print(a)
+# a[1:3] = ['a','b','c'] #[1, 'a', 'b', 'c']
+# print(a)
+
+# a = [1,2,3]
+# a[1] = ['a','b','c']
+# print(a)
+
+# a = [1,2,3]
+# a[1:2] = ['a','b','c']
+# print(a) #[1, 'a', 'b', 'c', 3]
+# a[1:3] = [] #[1, 'c', 3]
+# a[2] = 4
+# print(a)
+# del a[1]
+# print(a)
+#
+# b = 'a'
+# del b
+# print(b) #name 'b' is not defined
+
+# a = [1,2,3]
+# a.append(4)
+# print(a)
+# a.append([5,6])
+# print(a)
+# a = [1,4,3,2]
+# print(a)
+# a.sort()
+# print(a)
+# a = ['a','b','c']
+# a.reverse()
+# print(a)
+# a = [1,2,3]
+# print(a)
+# print(a.index(3)) #Return first index of value.
+
+# a = [1,2,3]
+# a.insert(5,4)
+# print(a)
+# a.insert(2,'a')
+# print(a)
+# a.remove(2)
+# print(a) #Remove first occurrence of value.
+#
+# a = [1,2,3]
+# print(a)
+# print(a.pop()) #Remove and return item at index (default last).
+# print(a)
+#
+# a = [1,2,3]
+# print(a.pop(1))
+# print(a)
+
+# a = [1,2,3,1]
+# print(a.count(1))
+
+# a = [1,2,3]
+# a.extend([4,5])
+# print(a)
+
+# a = {'a':1,'b':2,'c':3,'d':[4,5,6]}
+
+# a = {1: "z"}
+# a[2] = 'b'   # a[key] = value 수정/추가
+# print(a)
+# a[1] = 'a'
+# print(a)
+# a['name'] = 'yk'
+# print(a)
+# del a[1]
+# print(a)
+# a[3] = [1,2,3]
+# print(a)
+# del(a[3])
+# print(a)
+# print(len(a))
+# print(a['name'])
+
+# a = {[1,2]:'a'} #unhashable
+# a = {(1,2):'a'}
+# print(a[(1,2)])
+
+
 
 
 
