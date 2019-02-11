fe69bd307c9249323c6b28b91bc8259d6843b0c5
kimyk0120 <kimyk0120@hotmail.com>
2019-02-11 16:15:59 +0900

    python-basic

--- facam-python-basic/27th/27th_1.py
+++ facam-python-basic/27th/27th_1.py
@@ -2,10 +2,16 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
 
+f = open("../../user.properties",'r')
+user_id = f.readline().strip()
+user_pwd = f.readline().strip()
+# print("user_id : " + user_id)
+# print("user_pwd : " + user_pwd)
+
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 465
-SMTP_USER = "userEmail"
-SMTP_PASSWORD = "userPW"
+SMTP_USER = user_id
+SMTP_PASSWORD = user_pwd
 
 
 def send_mail(name, addr, subject , contents):
