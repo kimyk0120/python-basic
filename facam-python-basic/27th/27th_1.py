ee5ecfa95af1a260f6e0571d13791402d668bbe6
kimyk0120 <kimyk0120@hotmail.com>
2019-02-11 10:09:16 +0900

    python-basic

---
+++ facam-python-basic/27th/27th_1.py
@@ -0,0 +1,16 @@
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
+
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 465
+SMTP_USER = "userEmail"
+SMTP_PASSWORD = "userPW"
+
+def send_mail(name, addr, subject , contents):
+        msg = MIMEMultipart('alternative')
+
+        msg['From'] = SMTP_USER
+        msg['To'] = addr
+        msg['Subject'] = name + '님,' + subject
+
