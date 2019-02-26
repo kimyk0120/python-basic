23f70a4fbbb9b1ad365ce10e2847ef326e20812e
kimyk0120 <kimyk0120@hotmail.com>
2019-02-26 12:48:59 +0900

    python-basic

--- DeepLearningFromScratch/201902.py
+++ DeepLearningFromScratch/201902.py
@@ -1,11 +1,33 @@
-class Man:
-    def __init__(self, name):
-        self.name = name
-        print("Initialized")
+# class Man:
+#     def __init__(self, name):
+#         self.name = name
+#         print("Initialized")
+#
+#     def sayHell(self):
+#         print("Hello " + self.name)
+#
+#
+# m = Man("test")
+# m.sayHell()
 
-    def sayHell(self):
-        print("Hello " + self.name)
+# 201902
+import numpy as np
 
+x = np.array([1.0, 2.0, 3.0])
+y = np.array([2.0, 4.0, 6.0])
+print(x)
+print(type(x))
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
 
-m = Man("test")
-m.sayHell()
+a = np.array([[1, 2],[3, 4]])
+print(a)
+print(a.shape)
+print(a.dtype)
+
+b = np.array([[3, 0], [0, 6]])
+print(a + b)
+print(a * b)
+
