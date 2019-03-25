dc6a3109f12c17fa83ab17c49fe3725f93ef1da5
kimyk0120 <kimyk0120@hotmail.com>
2019-03-25 09:52:41 +0900

    python-basic

---
+++ facam-python-basic/WebAndMarketing/9th/9th_1.py
@@ -0,0 +1,26 @@
+'''
+    예외처리
+'''
+
+# print(int("test"))
+
+try:
+    # print(int("try"))
+    print(int("1234"))
+except Exception as e:
+    print(e)
+finally:
+    print("finally")
+
+
+
+
+
+
+
+
+
+
+
+
+
