73115be1403145eadc5213174df21e2e1f7b3b87
kimyk0120 <kimyk0120@hotmail.com>
2019-04-30 09:31:57 +0900

    python-basic

--- facam-python-basic/Advanced/12th/12th_1.py
+++ facam-python-basic/Advanced/12th/12th_1.py
@@ -1,7 +1,5 @@
-
 """
-    API
-
+    API DART http://dart.fss.or.kr/
 """
 import requests
 
@@ -12,8 +10,34 @@ import requests
 # http://dart.fss.or.kr/api/search.json?auth=xxx    json 응답
 
 f = open("dartAuthKey", 'r')
-authK = f.readline()
-print(authK)
+authKey = f.readline()
+# print(authKey)
+
+args = {
+    'start_dt': '20190301',
+    'end_dt': '20190430',
+    'sort': 'crp'
+}
+
+args_str = ''
+for k, v in args.items():
+    args_str += '&%s=%s' % (k, v)
+
+res = requests.get('http://dart.fss.or.kr/api/search.json?auth=%s%s' % (authKey, args_str))
+# print(res.status_code)
+# if res.status_code == 200:
+data = res.json()
+print(data)
+# print(data['err_code'])
+
+if data['err_code'] != '000':
+    print('err msg : ', data['err_msg'])
+else:
+    print("\n" + ("#" * 200) + "\n")
+    data_list = data['list']
+    for d in data_list:
+        print(d['crp_nm'])
+        # print(d)
 
 
 
