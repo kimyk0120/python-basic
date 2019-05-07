d545d5ef291cfa311d79b73793180bbd16d358fc
kimyk0120 <kimyk0120@hotmail.com>
2019-05-07 09:51:44 +0900

    python-basic

---
+++ facam-python-basic/Advanced/22th/22th-1.py
@@ -0,0 +1,54 @@
+from tkinter import *
+
+
+class GUIT():
+
+    def __init__(self):
+        self.tkhandler = Tk()
+        self.tkhandler.geometry('500x500')
+        self.tkhandler.title('facam online program')
+
+        self.label_title = Label(self.tkhandler, text='Hello World')
+        self.label_title.pack()
+
+        self.btn = Button(self.tkhandler, text='11st search', width=30, command=self.auto_11st)
+        self.btn.pack()
+
+        self.label_telegram = Label(self.tkhandler, text='telegram ID')
+        self.label_telegram.pack()
+
+        self.text_telegram = Text(self.tkhandler, width=40, height=1, relief=RIDGE, bd=1)
+        self.text_telegram.pack()
+
+        self.scroll = Scrollbar(self.tkhandler, orient='vertical')
+        self.lbox = Listbox(self.tkhandler, yscrollcommand=self.scroll.set)
+        self.scroll.config(command=self.lbox.yview)
+
+        self.scroll.pack(side='right', fill='y')
+        self.lbox.pack(side='left', fill='both',expand=True)
+
+        self.append_log("init")
+
+    def auto_11st(self):
+        self.append_log("11st example start")
+        import example11st
+        tid = self.text_telegram.get('1.0', END).strip()
+        example11st.do_auto(tid)
+        self.append_log('11st example end')
+
+    def append_log(self, msg):
+        import datetime
+        now = str(datetime.datetime.now())[11:-7]
+        self.lbox.insert(END, "[%s] %s" % (now, msg))
+        self.lbox.update()
+        self.lbox.see(END)
+
+    def run(self):
+        self.tkhandler.mainloop()
+
+
+
+g = GUIT()
+g.run()
+
+
---
+++ facam-python-basic/Advanced/22th/example11st.py
@@ -0,0 +1,89 @@
+"""
+    11번가 수집내용 변경됐을 시 메신져로 보내기
+"""
+
+from selenium import webdriver
+import datetime
+import os
+import telepot
+
+
+def do_auto(tid="testID"):
+    # selenium 크롬 headless 옵션
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    # 혹은 options.add_argument("--disable-gpu")
+
+    f = open("product.txt", 'r')
+    products = f.readlines() # 행 단위로 끊어서 list 형으로 반환
+    f.close()
+
+    prices= None
+    if os.path.exists('output.csv'):
+        # output = open('output.csv', 'w')
+        output = open('output.csv', 'r')
+        prices = output.readlines().pop().strip().split(',')
+        output.close()
+
+    print(prices)
+    # print(products)
+
+    driver = webdriver.Chrome("chromedriver",options=options)
+
+    try:
+        idx = 1
+        header = ['']
+        data = [str(datetime.datetime.now())]
+        diff = []
+        for index, url in enumerate(products):
+            print(url)
+            url = url.strip()
+            driver.get(url)
+
+            elem = driver.find_element_by_class_name("heading")
+            elem = elem.find_element_by_tag_name("h2")
+            name = elem.text
+            print("name : ", name)
+
+            elem = driver.find_element_by_class_name("prdc_default_info")
+            elem = elem.find_element_by_class_name("sale_price")
+            price = elem.text.replace(",","")
+            print("price : ", price)
+
+            if prices and price != prices[idx]:
+                diff.append((name, prices[idx], price))
+
+            header.append(name)
+            data.append(price.replace(",", ""))
+
+            idx += 1
+
+        print("#" * 120)
+        print(diff)
+
+        if diff:
+            bot = telepot.Bot('850452568:AAHw3xxLmmZX8kkxwIt0LcutMVud_9pzAqg')
+            keyF = open("idKey", 'r')
+            telegram_id = keyF.readline()
+
+            msg = ''
+            for info in diff:
+                msg += '- %s\n%s => %s\n' % info
+            bot.sendMessage(telegram_id, msg)
+
+        print(header)
+        if not os.path.exists('output.csv'):
+            output.write(','.join(header)+"\n")
+
+        output.write(",".join(data)+'\n')
+
+    # .try
+    except Exception as e:
+        print(e)
+    finally:
+        driver.quit()
+        output.close()
+        f.close()
+
